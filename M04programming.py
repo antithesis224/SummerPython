64bff15c3fafefb548dbc72ffc96a9879d1cd615
antithesis224 <129119982+antithesis224@users.noreply.github.com>
2023-07-02 15:14:53 -0400

    Add files via upload

---
+++ M04programming.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+from zoo import hours
+
+def hours():
+    print('Open 9-5 daily')
+
+
+# In[13]:
+
+
+import zoo
+zoo.hours()
+
+
+# In[14]:
+
+
+import zoo as menagerie
+menagerie.hours()
+
